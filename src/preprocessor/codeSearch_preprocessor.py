deca76df730b34240b74c1ce95f4ae0d9f6b5871
Profir-Petru Partachi <anon@nymo.us>
2020-03-25 18:46:14 +0000

    WIP: converting CodeSearch data to POSIT++ expected output

--- src/preprocessor/codeSearch_preprocessor.py
+++ src/preprocessor/codeSearch_preprocessor.py
@@ -1,9 +1,13 @@
+from collections import deque
+
 # Used to parse code to ASTs
 import antlr4
 # Enables reading the corpus
 import json_lines as jl
 
 # Individual languages that we want to parse
+from nltk import sent_tokenize, casual_tokenize
+
 from src.antlr4_language_parsers.golang.GoLexer import GoLexer as gol
 from src.antlr4_language_parsers.golang.GoParser import GoParser as gop
 from src.antlr4_language_parsers.java.Java9Lexer import Java9Lexer as javal
@@ -29,6 +33,10 @@ languages = [
     'ruby',
 ]
 
+natural_languages = [
+    'English',
+]
+
 # language
 location_format = 'H:\\CodeSearch\\%s\\'
 # language
@@ -52,7 +60,7 @@ def parse_java(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = javap(stream)
     tree = parser.methodDeclaration()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_javadoc(entry):
@@ -61,7 +69,7 @@ def parse_javadoc(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = javadocp(stream)
     tree = parser.documentation()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_golang(entry):
@@ -70,7 +78,7 @@ def parse_golang(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = gop(stream)
     tree = parser.methodDecl()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_js(entry):
@@ -79,7 +87,7 @@ def parse_js(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = jsp(stream)
     tree = parser.program()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_php(entry):
@@ -88,7 +96,7 @@ def parse_php(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = phpp(stream)
     tree = parser.functionDeclaration()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_python(entry):
@@ -97,7 +105,7 @@ def parse_python(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = pyp(stream)
     tree = parser.file_input()
-    return tree.toStringTree(recog=parser)
+    return tree
 
 
 def parse_ruby(entry):
@@ -106,25 +114,71 @@ def parse_ruby(entry):
     stream = antlr4.CommonTokenStream(lexer)
     parser = rubyp(stream)
     tree = parser.prog()
-    return tree.toStringTree(recog=parser)
+    return tree
+
+
+def extract_name_from_terminal(terminal_node):
+    return str(type(terminal_node.parentCtx)).split('.')[-1][:-9]
+
+
+def ast_to_tagged_list(tree):
+    result = list()
+    visit = deque([tree])
+
+    while len(visit) > 0:
+        current = visit.pop()
+        if current.children is not None:
+            for child in current.children:
+                if isinstance(child, antlr4.tree.Tree.TerminalNodeImpl):
+                    tag = extract_name_from_terminal(child)
+                    result.append((child.symbol.text, tag))
+                else:
+                    visit.append(child)
+
+    return [l for l in reversed(result)]
+
+
+def parse_docstring(entry, language, code_context):
+    result = list()
+    if language == 'java':
+        docstring_parse = parse_javadoc(entry)
+        tagged_list = ast_to_tagged_list(docstring_parse)
+        # TODO: Reconstruct sentences for NLTK.
+        for snippet_or_tok, tag in tagged_list:
+            if tag == "":
+                toks = [casual_tokenize(s) for s in sent_tokenize(snippet_or_tok)]
+                for tok, tag_ in toks:
+                    if tok in code_context.keys():
+                        context = code_context[tok]
+                        result.append((tok, ('English', context[-1]+{'English': tag_})))
+            else:
+                # TODO: Output the code-entity tag
+                if snippet_or_tok in code_context.keys():
+                    context = code_context[snippet_or_tok]
+                    result.append((snippet_or_tok, ('English', context[-1]+{'English': tag})))
+    else:
+        toks = [casual_tokenize(s) for s in sent_tokenize(entry['docstring'])]
+        for tok, tag_ in toks:
+            if tok in code_context.keys():
+                context = code_context[tok]
+                result.append((tok, ('English', context[-1] + {'English': tag_})))
+
+    return result
 
 
 def read_corpus_file(location, language):
     with jl.open(location) as f:
         for entry in f:
             ast = globals()["parse_%s" % language](entry)
-            if language == 'java':
-                docstring_parse = parse_javadoc(entry)
-            else:
-                # TODO: Process the line using NLTK
-                pass
-
-            # TODO: Do something with AST and docstring to save for POSIT
+            tagged_code_list = [
+                (tok, (language, {l: tag if l == language else '' for l in languages + natural_languages}))
+                for tok, tag in ast_to_tagged_list(ast)]
+            tagged_docstring_list = parse_docstring(entry, language, dict(tagged_code_list))
 
 
 def main():
-    location = (location_format + jsonl_location_format) % ('ruby', 'ruby', 'train', 'ruby', 'train', 0)
-    read_corpus_file(location, 'ruby')
+    location = (location_format + jsonl_location_format) % ('java', 'java', 'train', 'java', 'train', 0)
+    read_corpus_file(location, 'java')
 
 
 if __name__ == '__main__':
