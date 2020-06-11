1ccd2f312666e360fcb247edde0915fffd27f127
Profir-Petru Partachi <pppi@users.noreply.github.com>
2020-06-11 09:42:32 +0100

    Only generate the SO CSV once, after, reuse it

--- src/preprocessor/so_to_pandas.py
+++ src/preprocessor/so_to_pandas.py
@@ -66,12 +66,16 @@ def tokenize_SO_row(row_, language, tag_name='body'):
 
 
 def SO_to_pandas(location):
-    result_df = pd.DataFrame(columns=['PostIdx', 'Token', 'Language', 'Span', 'Context'])
-    for pidx, (row, language) in enumerate(parse_stackoverflow_posts(location)):
-        toks = tokenize_SO_row(row, language)
-        temp_df = pd.DataFrame(toks, columns=['Token', 'Language', 'Span', 'Context'])
-        temp_df['PostIdx'] = pd.Series([pidx] * len(temp_df.index))
-        result_df = result_df.append(temp_df, ignore_index=True, sort=False)
+    try:
+        result_df = pd.read_csv(location[:-len('xml')] + 'csv')
+    except FileNotFoundError:
+        result_df = pd.DataFrame(columns=['PostIdx', 'Token', 'Language', 'Span', 'Context'])
+        for pidx, (row, language) in enumerate(parse_stackoverflow_posts(location)):
+            toks = tokenize_SO_row(row, language)
+            temp_df = pd.DataFrame(toks, columns=['Token', 'Language', 'Span', 'Context'])
+            temp_df['PostIdx'] = pd.Series([pidx] * len(temp_df.index))
+            result_df = result_df.append(temp_df, ignore_index=True, sort=False)
+        result_df.to_csv(location[:-len('xml')] + 'csv')
 
     return result_df
 
