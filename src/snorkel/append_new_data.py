99b760363a4be776b5e4d6bdd2d5665049ed5c4a
Profir-Petru Partachi <pppi@users.noreply.github.com>
2020-08-15 06:57:25 +0100

    Fix result agglomeration

--- src/snorkel/append_new_data.py
+++ src/snorkel/append_new_data.py
@@ -22,10 +22,10 @@ from src.snorkel.weak_labellers import *
 def parallelize_dataframe(df, func, n_cores=4):
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
-    df = pd.concat(pool.map(func, df_split))
+    result = np.r_[list(pool.map(func, df_split))]
     pool.close()
     pool.join()
-    return df
+    return result
 
 def main(argv):
     location = argv[0]
