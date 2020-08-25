bcc1685cfe993ce5d129001d35985516a0657fa3
Profir-Petru Partachi <pppi@users.noreply.github.com>
2020-08-25 13:21:36 +0100

    Update the classifier code to handle the preprocessing of labels

--- src/source_identification/basic_classifier.py
+++ src/source_identification/basic_classifier.py
@@ -3,23 +3,28 @@ import sys
 
 import numpy as np
 from daal4py.sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
 
 def main(argv):
     name = 'Random Forest Classifier'
     clf = RandomForestClassifier(max_depth=15, n_estimators=100)
-    sources = argv[1:]
+    sources = argv
     Xy = list()
+    le = preprocessing.LabelEncoder()
+    le.fit(sources)
     for source in sources:
         for file in glob.glob('./data/source_identification/%s/feature_vector_*.npz' % source):
             loaded = np.load(file)
-            fv = np.concatenate(loaded['tags'], loaded['lids'])
+            tags = loaded['tags']
+            lids = loaded['lids']
+            fv = np.concatenate((tags, lids))
             Xy.append((fv, source))
 
     X, y = list(zip(*Xy))
     X = np.asarray(X)
-    y = np.asarray(y)
+    y = le.transform(np.asarray(y))
 
     split_ratio = 0.4
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_ratio, random_state=42)
