619236bfffdc4d246f3bc96f1de5b3aa54a94e75
Profir-Petru Partachi <pppi@users.noreply.github.com>
2021-01-04 14:55:51 +0200

    Report accuracy at 3 as well

--- src/process_multilang.py
+++ src/process_multilang.py
@@ -13,12 +13,17 @@ def most_frequent(lst):
     return max(set(lst), key=lst.count)
 
 
+def least_frequent(lst):
+    return min(set(lst), key=lst.count)
+
+
 def process_data(model, target_data, stackoverflow=False):
     if stackoverflow:
         source = tokenise_SO(target_data, 75000, 76000, True)
     else:
         source = tokenise_lkml(target_data)
     lid_hits = list()
+    lid_hits_at_3 = list()
     for sents_raw, tags in source:
         all_lids = list()
         for words_raw in sents_raw:
@@ -27,14 +32,24 @@ def process_data(model, target_data, stackoverflow=False):
                 all_lids += preds[-1]
                 with open(f"./results/multilang/for_manual_investigation_{stackoverflow}.txt", 'a') as f:
                     f.write('\n'.join(['%s\t%s\t%s' % (w, str(t), l) for w, (t, l) in zip(words_raw, zip(*preds))]))
+                    f.write('\n')
         all_lids = [lid for lid in all_lids if lid != 'English']
         lid_pred = most_frequent(all_lids) if len(all_lids) > 0 else ''
-        if any([t in tags for t in PLs_trained_on]):
+
+        while len(set(all_lids)) > 3:
+            exclude = least_frequent(all_lids)
+            all_lids = [lid for lid in all_lids if lid != exclude]
+
+        top_3 = set(all_lids)
+
+        if any([t in tags for t in PLs_trained_on]) and len(lid_pred) > 0:
             lid_hits.append(1 if lid_pred in tags else 0)
+            lid_hits_at_3.append(1 if any([lid in tags for lid in top_3]) else 0)
         with open(f"./results/multilang/for_manual_investigation_{stackoverflow}.txt", 'a') as f:
             f.write('\n\n' + ''.join(['_'] * 80) + '\n\n')
     print(f"For posts over trained languages, we guessed correctly the user tag in {np.mean(lid_hits):2.3f} cases.")
-
+    print(f"For posts over trained languages, we guessed correctly in top 3 "
+          f"the user tag in {np.mean(lid_hits):2.3f} cases.")
 
 def main():
     # create instance of config
